0f7844e5e74a18d7b2a78ce7c1e890683f4c8ef1
Niyat Hannibal <niyathannibal@gmail.com>
2026-08-23 19:14:33 +0300

    Add CORS configuration

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import workspace, task, user, auth, membership
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(user.router)
